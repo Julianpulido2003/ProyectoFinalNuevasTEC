c59d3c1fb4a43e23c2b081f66e9ca23d5aff773a
julianpulido2003 <julianpulido2014@gmail.com>
2022-05-28 21:52:54 -0500

    Se agrega el Proyecto

---
+++ Clase3CorteBlazor/Data/Proveedor.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Clase3CorteBlazor.Data
+{
+    public class Proveedor
+    {
+        [Key]
+
+        public int idproveedor { get; set; }
+        public string identificacion{ get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }        
+    }
+}
---
+++ Clase3CorteBlazor/Data/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Clase3CorteBlazor.Data
+{
+    public class Usuario
+    {
+        [Key]
+
+        public int idusuario { get; set; }
+        public string identificacion{ get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }        
+    }
+}
